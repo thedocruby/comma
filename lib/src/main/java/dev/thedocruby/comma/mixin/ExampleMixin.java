588389df98dc836b0f7acab97e550f5e1ff31f1e
Dr. Rubisco <76263371+thedocruby@users.noreply.github.com>
2022-01-05 15:07:27 -0500

    Configure example mod for COMMA development

--- lib/src/main/java/dev/thedocruby/comma/mixin/ExampleMixin.java
+++ lib/src/main/java/dev/thedocruby/comma/mixin/ExampleMixin.java
@@ -1,7 +1,7 @@
-package net.fabricmc.example.mixin;
+package dev.thedocruby.comma.mixin;
 
-import net.fabricmc.example.ExampleMod;
-import net.minecraft.client.gui.screen.TitleScreen;
+import dev.thedocruby.comma.CommaMod;
+import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		CommaMod.LOGGER.info("This line is printed by a COMMA Config API mixin!");
 	}
 }
